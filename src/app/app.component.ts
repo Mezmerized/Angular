a0d1432859a35860309148a83fa1aff0db8dbd4c
Mezmerized <maximilianstarke@icloud.com>
2020-01-23 06:16:23 +0100

    +

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { HttpService } from './http.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HttpService]
 })
 export class AppComponent {
-  title = 'http-app';
+  people: any = [];
+  films: any = [];
+
+  constructor(private httpService: HttpService) {}
+
+  onGetPeople() {
+    this.httpService.getPeople()
+        .subscribe(
+       data => {console.log(data);
+       // this.people = data;
+       })
+  }
+
+  onGetFilms() {
+    this.httpService.getFilms()
+        .subscribe(
+       data => {console.log(data);
+       // this.films = data;
+       })
+  }
 }
---
+++ src/app/http.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { map, catchError, tap } from 'rxjs/operators';
+
+@Injectable()
+export class HttpService {
+    constructor (private httpClient: HttpClient) {}
+
+    getPeople() {
+        return this.httpClient.get('https://swapi.co/api/people/')
+        }
+
+    getFilms() {
+        return this.httpClient.get('https://swapi.co/api/films/')
+        }
+
+}
+
+
+
+
+
+
